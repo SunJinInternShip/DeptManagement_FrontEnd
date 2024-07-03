70e185bf6ef591a0a59708477e96b01b2d0ee834
Ourumo <ss8638@naver.com>
2024-07-04 08:51:16 +0900

    feat: Login, SampleData 연동 및 기능 간단히 추가

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { userData } from '../utils/SampleData';
 
 export default function Login() {
   const [id, setId] = React.useState<string>('');
@@ -6,15 +7,23 @@ export default function Login() {
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    alert(`아이디: ${id}\n비밀번호: ${password}`);
+    userData.map((user) => {
+      if(user.userId === id) {
+        if(user.password === password) {
+          alert("로그인");
+        }
+      }
+    })
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div>
         <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-          <input type='text' placeholder='id' onChange={(e: any) => {setId(e.target.value)}}/>
-          <input type='text' placeholder='password' onChange={(e: any) => {setPassword(e.target.value)}}/>
+          <input type='text' placeholder='id' required
+           onChange={(e: any) => {setId(e.target.value)}}/>
+          <input type='password' placeholder='password' required
+           onChange={(e: any) => {setPassword(e.target.value)}}/>
           <input type='submit' value="로그인"/>
         </form>
       </div>
