88f4c74f22b099eb40606c1ae1ddfd9d06552d56
Ourumo <ss8638@naver.com>
2024-07-04 08:07:36 +0900

    sampleData 추가

---
+++ src/utils/SampleData.js
@@ -0,0 +1,55 @@
+export const userData = [
+  {
+    "key": 1,
+    "departmentKey": "1000",
+    "userName": "어드민",
+    "userId": "admin",
+    "password": "admin",
+    "role": "관리자",
+    "phoneNumber": "1000"
+  },
+  {
+    "key": 2,
+    "departmentKey": "1234",
+    "userName": "김테스트",
+    "userId": "test",
+    "password": "test",
+    "role": "사원",
+    "phoneNumber": "010-1234-5678"
+  }
+]
+export const productData = [
+  {
+    "key": 1,
+    "userKey": 2,
+    "reqData": 123,
+    "type": "비품",
+    "price": 10000,
+    "productName": "비품A",
+    "quantity": 3,
+    "totalPrice": 30000,
+    "status": "대기",
+    "processDate": null
+  },
+  {
+    "key": 2,
+    "userKey": 2,
+    "reqData": 456,
+    "type": "기타",
+    "price": 1000000,
+    "productName": "기타A",
+    "quantity": 1,
+    "totalPrice": 1000000,
+    "status": "반려",
+    "processDate": 458
+  }
+]
+export const receiptData = [
+  {
+    "key": 1,
+    "userKey": 2,
+    "storeName": "AAA",
+    "rTotalPrice": 100000,
+    "reqDate": 1234
+  }
+]
