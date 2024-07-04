8cb8845e2d4a0e255d4334bb3881a980237cbc48
Ourumo <ss8638@naver.com>
2024-07-04 16:29:29 +0900

    design: 스피너 디자인 수정, refactoring 필요 -> 가운데 정렬 X

--- src/pages/DepartmentMain.tsx
+++ src/pages/DepartmentMain.tsx
@@ -7,7 +7,65 @@ export default function DepartmentMain() {
   //<input type='button' value="버튼" onClick={() => {setState(!state)}}/> {state? Modal() : ""}
   //{Spinner()}
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+      {Spinner()}
+      <div style={{ display: 'flex', justifyContent: 'space-between', width: '100%' }}>
+        <span>로고</span>
+        <span>test님 반갑습니다!</span>
+      </div>
+      <div style={{ textAlign: 'center' }}>
+        YYY 부서의 총 사용 금액: 123,456,789 원
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+        <input type='button' value="버튼"/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <table style={{ width: '100%' }}>
+          <tr>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+          </tr>
+          <tr>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+          </tr>
+        </table>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'flex-end', width: '100%' }}>
+        <input type='button' value="버튼"/>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <table style={{ width: '100%' }}>
+          <tr>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+          </tr>
+          <tr>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+            <td>123</td>
+          </tr>
+        </table>
+      </div>
     </div>
   );
 }
