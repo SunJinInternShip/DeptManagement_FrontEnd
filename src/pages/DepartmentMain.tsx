0a3a233c0b4ecfad42fd01978126ed785c6b26f1
Ourumo <ss8638@naver.com>
2024-07-04 15:22:15 +0900

    feat: 로딩 중에 보여 줄 스피너 추가

--- src/pages/DepartmentMain.tsx
+++ src/pages/DepartmentMain.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import Modal from '../components/ProductModal';
+import Spinner from '../components/Spinner';
 
 export default function DepartmentMain() {
   const [state, setState] = React.useState<boolean>(false);
   //<input type='button' value="버튼" onClick={() => {setState(!state)}}/> {state? Modal() : ""}
-  
+  //{Spinner()}
   return (
-    <div>
-      
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
     </div>
   );
 }
