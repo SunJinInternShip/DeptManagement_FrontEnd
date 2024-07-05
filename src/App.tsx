1e0aeccc062ea3e23b8e878d20808d2d0dda5ebb
Ourumo <ss8638@naver.com>
2024-07-05 16:55:14 +0900

    app에 adminmain 연결

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import DepartmentMain from "./pages/DepartmentMain";
+import AdminMain from "./pages/AdminMain";
 
 // 로그인 유무(토큰의 유무)에 따라 로그인/홈 페이지로 이동
 function App() {
@@ -11,7 +12,7 @@ function App() {
         <Route path="/" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<DepartmentMain />} />
-        <Route path="/home/admin" element={<DepartmentMain />} />
+        <Route path="/home/admin" element={<AdminMain />} />
       </Routes>
     </BrowserRouter>
   );
