ec495860344cd222332bdac8223a4cd93463a129
Ourumo <ss8638@naver.com>
2024-07-05 10:49:57 +0900

    feat: 로그인 기능 - 백엔드와 연결

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,33 +1,53 @@
+import axios from 'axios';
 import * as React from 'react';
-import { userData } from '../utils/SampleData';
 import { useNavigate } from "react-router-dom";
 
+interface User {
+  id: string;
+  password: string;
+}
+
 export default function Login() {
-  const [id, setId] = React.useState<string>('');
-  const [password, setPassword] = React.useState<string>('');
+  const [user, setUser] = React.useState<User>({
+    id: '',
+    password: ''
+  });
 
   const navigate = useNavigate();
 
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setUser((user: User) => ({
+      ...user,
+      [name]: value
+    }));
+  }
+
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    // /login
-    userData.map((user) => {
-      if(user.userId === id) {
-        if(user.password === password) {
-          alert("로그인");
-        }
-      }
-    })
+    try {
+      const res = await axios.post(`${process.env.REACT_APP_SERVER_URL}/login`, {
+        "loginId": user.id,
+        "password": user.password
+      });
+      //console.log(res.data); // 토큰 값 주의!!!
+      alert("로그인 성공!");
+    } catch (error) {
+      console.log(error);
+    }
+    navigate("/");
   };
 
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
       <div>
         <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
-          <input type='text' placeholder='id' required
-           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setId(e.target.value)}}/>
-          <input type='password' placeholder='password' required
-           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {setPassword(e.target.value)}}/>
+          <input type='text' placeholder='id' name='id'
+           value={user.id} required
+           onChange={handleChange}/>
+          <input type='password' placeholder='password' name='password'
+           value={user.password} required
+           onChange={handleChange}/>
           <input type='submit' value="로그인"/>
           <button onClick={() => {navigate("/register")}}>회원가입</button>
         </form>
