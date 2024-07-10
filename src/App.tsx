0c87f0f222dd7e7f03b2be266b9edc189891944a
Ourumo <ss8638@naver.com>
2024-07-11 08:13:12 +0900

    comment: 주석 제거

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Register from "./pages/Register";
 import DepartmentMain from "./pages/DepartmentMain";
 import AdminMain from "./pages/AdminMain";
 
-// 로그인 유무(토큰의 유무)에 따라 로그인/홈 페이지로 이동
 function App() {
   return (
     <BrowserRouter>
